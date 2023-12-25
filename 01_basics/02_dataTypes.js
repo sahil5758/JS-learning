4c92d69044821831f1aaebf17bd660a9de0ca0ea
SAHIL <84861578+sahil5758@users.noreply.github.com>
2023-12-25 19:39:42 +0000

    DataTypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+" use strict " ;  // Treat all js code as newer version 
+
+ // alert("hello")  // we are using node js not browser 
+
+ console.log(3
+    +              // bad practice
+    3)  // code readability should be high 
+
+    let  Name = " sahil"  // string datatype
+    let age = 18         //  number datatype
+    let isMarried = false // boolean datatype
+
+/*
+DATATYPES :-
+Data types describe the different types or kinds of data that we're gonna be working with and storing in variables
+String.
+Number.
+Bigint.
+Boolean.
+Undefined.
+Null.
+Symbol.
+Object.
+*/
+
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined);
+console.log( typeof Name );
