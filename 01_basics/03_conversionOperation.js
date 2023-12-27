6820fb5014bf4d4d8b58aae3adbcb63bb19b93b2
SAHIL <84861578+sahil5758@users.noreply.github.com>
2023-12-27 19:48:02 +0000

    Operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,45 @@ let someNumber = 33 ;
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let value = 3 ;
+let negValue = -value 
+
+console.log(negValue);
+
+/*
+// mathematic operators 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+*/
+
+let str1 ="hello";
+let str2 = " hitesh"
+let str3 = str1+str2;
+
+/*
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2");
+console.log((3+4)*5%3);
+console.log(true);
+console.log(+true);
+console.log(+"");
+*/
+
+let num1 , num2 , num3 ;
+num1 = num2 = num3= 2+2 ; //  bad  readability
+
+let gamecounter = 100 ;
+console.log(gamecounter++);// print  100  then 101
+
+++gamecounter; // 101 + 1 print 102
+console.log(gamecounter);
+
