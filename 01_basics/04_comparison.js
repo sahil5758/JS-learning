e7b70680dac3c9d502a4db212741851f1c157a16
SAHIL <84861578+sahil5758@users.noreply.github.com>
2023-12-30 18:10:35 +0000

    comparison and equality operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+
+console.log( 2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+// equality and comparing operator work differently...
+console.log("2">1);
+console.log("02">1);
+console.log(null>0);
+console.log(null == 0);
+console.log(null >=0);
+console.log(undefined == 0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+// strict check ===
+console.log("2"==2);
+console.log("2"===2);
